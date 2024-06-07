9966d3b8a459fa8f8b432f8409c27b9ccc82af9e
Kristina Stoyanova <44975817+KristinaSS@users.noreply.github.com>
2024-06-07 17:26:14 +0300

    endpoint can be called from frontend

--- src/main/java/com/simp/me/controller/SkinController.java
+++ src/main/java/com/simp/me/controller/SkinController.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 public class SkinController implements SkinApi {
 
