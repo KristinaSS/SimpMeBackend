f8d0026544778ac9cdd4d196b2c57659c426ff5a
Kristina Stoyanova <44975817+KristinaSS@users.noreply.github.com>
2024-06-07 17:26:03 +0300

    endpoint can be called from frontend

--- src/main/java/com/simp/me/controller/SkinController.java
+++ src/main/java/com/simp/me/controller/SkinController.java
@@ -3,6 +3,7 @@ package com.simp.me.controller;
 import com.simp.me.service.SkinService;
 import org.openapitools.api.SkinApi;
 import org.openapitools.model.AccountDTO;
+import org.openapitools.model.AccountRequest;
 import org.openapitools.model.AddSkinDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,8 @@ public class SkinController implements SkinApi {
     private SkinService skinService;
 
     @Override
-    public ResponseEntity<AccountDTO> getALlSkins(UUID uuid) {
-        return ResponseEntity.ok(skinService.getAllSkins(uuid));
+    public ResponseEntity<AccountDTO> getALlSkins(AccountRequest uuid) {
+        return ResponseEntity.ok(skinService.getAllSkins(uuid.getId()));
     }
 
     @Override
